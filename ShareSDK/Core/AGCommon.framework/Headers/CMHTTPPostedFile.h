73e1f470da6fec268895dae1618e98b8caf10292
ljh <ljh@LiuJinghuang-2.local>
2014-10-09 19:44:12 +0800

    V2.9.6
    V2.9.6

--- ShareSDK/Core/AGCommon.framework/Headers/CMHTTPPostedFile.h
+++ ShareSDK/Core/AGCommon.framework/Headers/CMHTTPPostedFile.h
@@ -1,3 +1,14 @@
+///#begin zh-cn
+//
+//  Created by ShareSDK.cn on 13-1-14.
+//  官网地址:http://www.ShareSDK.cn
+//  技术支持邮箱:support@sharesdk.cn
+//  官方微信:ShareSDK   （如果发布新版本的话，我们将会第一时间通过微信将版本更新内容推送给您。如果使用过程中有任何问题，也可以通过微信与我们取得联系，我们将会在24小时内给予回复）
+//  商务QQ:4006852216
+//  Copyright (c) 2013年 ShareSDK.cn. All rights reserved.
+//
+///#end
+///#begin en
 //
 //  Created by ShareSDK.cn on 13-1-14.
 //  Website:http://www.ShareSDK.cn
@@ -6,11 +17,20 @@
 //  Business QQ:4006852216
 //  Copyright (c) 2013年 ShareSDK.cn. All rights reserved.
 //
+///#end
+
 #import <Foundation/Foundation.h>
 
+///#begin zh-cn
+/**
+ *	@brief	HTTP上传文件信息
+ */
+///#end
+///#begin en
 /**
  *	@brief	HTTP Posted File.
  */
+///#end
 @interface CMHTTPPostedFile : NSObject
 {
 @private
@@ -20,26 +40,66 @@
     NSData *_fileData;
 }
 
+///#begin zh-cn
+/**
+ *	@brief	文件名称
+ */
+///#end
+///#begin en
 /**
  *	@brief	File name.
  */
+///#end
 @property (nonatomic,readonly) NSString *fileName;
 
+///#begin zh-cn
+/**
+ *	@brief	内容类型
+ */
+///#end
+///#begin en
 /**
  *	@brief	Content type.
  */
+///#end
 @property (nonatomic,readonly) NSString *contentType;
 
+///#begin zh-cn
+/**
+ *	@brief	文件数据
+ */
+///#end
+///#begin en
 /**
  *	@brief	File data.
  */
+///#end
 @property (nonatomic,readonly) NSData *fileData;
 
+///#begin zh-cn
+/**
+ *	@brief	内容传输编码
+ */
+///#end
+///#begin en
 /**
  *	@brief	Transfer encoding.
  */
+///#end
 @property (nonatomic,readonly) NSString *transferEncoding;
 
+///#begin zh-cn
+/**
+ *	@brief	初始化上传文件
+ *
+ *  @param  fileName    文件名称
+ *	@param 	data 	文件数据
+ *  @param  contentType 内容类型
+ *
+ *	@return	上传文件信息
+ */
+///#end
+///#begin en
 /**
  *	@brief	Initialize posted file.
  *
@@ -49,8 +109,22 @@
  *
  *	@return	Posted file object.
  */
+///#end
 - (id)initWithFileName:(NSString *)fileName data:(NSData *)data contentType:(NSString *)contentType;
 
+///#begin zh-cn
+/**
+ *	@brief	初始化上传文件
+ *
+ *  @param  fileName    文件名称
+ *	@param 	data 	文件数据
+ *  @param  contentType 内容类型
+ *  @param  transferEncoding     传输编码
+ *
+ *	@return	上传文件信息
+ */
+///#end
+///#begin en
 /**
  *	@brief	Initialize posted file.
  *
@@ -61,11 +135,23 @@
  *
  *	@return	Posted file object.
  */
+///#end
 - (id)initWithFileName:(NSString *)fileName
                   data:(NSData *)data
            contentType:(NSString *)contentType
       transferEncoding:(NSString *)transferEncoding;
 
+///#begin zh-cn
+/**
+ *	@brief	初始化上传文件
+ *
+ *	@param 	path 	文件路径
+ *  @param  contentType 内容类型
+ *
+ *	@return	上传文件信息
+ */
+///#end
+///#begin en
 /**
  *	@brief	Initialize posted file.
  *
@@ -74,6 +160,7 @@
  *
  *	@return	Posted file object.
  */
+///#end
 - (id)initWithFilePath:(NSString *)path contentType:(NSString *)contentType;
 
 
--- ShareSDK/Core/AGCommon.framework/Headers/CMImageHeaderTableView.h
+++ ShareSDK/Core/AGCommon.framework/Headers/CMImageHeaderTableView.h
@@ -1,3 +1,14 @@
+///#begin zh-cn
+//
+//  Created by ShareSDK.cn on 13-1-14.
+//  官网地址:http://www.ShareSDK.cn
+//  技术支持邮箱:support@sharesdk.cn
+//  官方微信:ShareSDK   （如果发布新版本的话，我们将会第一时间通过微信将版本更新内容推送给您。如果使用过程中有任何问题，也可以通过微信与我们取得联系，我们将会在24小时内给予回复）
+//  商务QQ:4006852216
+//  Copyright (c) 2013年 ShareSDK.cn. All rights reserved.
+//
+///#end
+///#begin en
 //
 //  Created by ShareSDK.cn on 13-1-14.
 //  Website:http://www.ShareSDK.cn
@@ -6,12 +17,21 @@
 //  Business QQ:4006852216
 //  Copyright (c) 2013年 ShareSDK.cn. All rights reserved.
 //
+///#end
+
 #import <UIKit/UIKit.h>
 #import "CMImageView.h"
 
+///#begin zh-cn
+/**
+ *	@brief	带图片头部的表格视图
+ */
+///#end
+///#begin en
 /**
  *	@brief	Image Header Table View.
  */
+///#end
 @interface CMImageHeaderTableView : UIView <UITableViewDelegate, UITableViewDataSource>
 {
 @private
@@ -24,56 +44,128 @@
     id _dataSource;
 }
 
+///#begin zh-cn
+/**
+ *	@brief	表格委托
+ */
+///#end
+///#begin en
 /**
  *	@brief	Table view delegate.
  */
+///#end
 @property (nonatomic,assign) id<UITableViewDelegate> delegate;
 
+///#begin zh-cn
+/**
+ *	@brief	数据源
+ */
+///#end
+///#begin en
 /**
  *	@brief	Table view data source.
  */
+///#end
 @property (nonatomic,assign) id<UITableViewDataSource> dataSource;
 
+///#begin zh-cn
+/**
+ *	@brief	表头图片
+ */
+///#end
+///#begin en
 /**
  *	@brief	Header image.
  */
+///#end
 @property (nonatomic,retain) UIImage *headerImage;
 
+///#begin zh-cn
+/**
+ *	@brief	表头高度
+ */
+///#end
+///#begin en
 /**
  *	@brief	Header height.
  */
+///#end
 @property (nonatomic) CGFloat headerHeight;
 
+///#begin zh-cn
+/**
+ *	@brief	表格背景颜色
+ */
+///#end
+///#begin en
 /**
  *	@brief	Table background color.
  */
+///#end
 @property (nonatomic,retain) UIColor *tableBackgroundColor;
 
+///#begin zh-cn
+/**
+ *	@brief	表格背景图片
+ */
+///#end
+///#begin en
 /**
  *	@brief	Table background image.
  */
+///#end
 @property (nonatomic,retain) UIImage *tableBackgroundImage;
 
+///#begin zh-cn
+/**
+ *	@brief	分隔线颜色
+ */
+///#end
+///#begin en
 /**
  *	@brief	Separator color.
  */
+///#end
 @property (nonatomic,retain) UIColor *separatorColor;
 
+///#begin zh-cn
+/**
+ *	@brief	分隔线样式
+ */
+///#end
+///#begin en
 /**
  *	@brief	Separator style.
  */
+///#end
 @property (nonatomic) UITableViewCellSeparatorStyle separatorStyle;
 
+///#begin zh-cn
+/**
+ *	@brief	行高度
+ */
+///#end
+///#begin en
 /**
  *	@brief	Row height.
  */
+///#end
 @property (nonatomic) CGFloat rowHeight;
 
+///#begin zh-cn
+/**
+ *	@brief	添加表头子视图
+ *
+ *	@param 	subview 	子视图
+ */
+///#end
+///#begin en
 /**
  *	@brief	Add header subview.
  *
  *	@param 	subview 	Subview.
  */
+///#end
 - (void)addHeaderSubview:(UIView *)subview;
 
 
